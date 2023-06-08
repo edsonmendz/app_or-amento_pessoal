5358dd5d319ddc9cf0a3db887ca05df3594fc03a
edsonmendz <japaact@gmail.com>
2023-06-08 19:59:56 -0300

    Orçamento Pessoal Finalizado

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ class Bd {
 			if (despesa === null) {			
 				continue
 			}
+			despesa.id = i
 			despesas.push(despesa)
 		}
 		return despesas
@@ -89,6 +90,10 @@ class Bd {
 		}
 		return despesasFiltradas
 	}	
+
+	remover(id) {
+		localStorage.removeItem(id)
+	}
 }
 
 	let bd = new Bd ()
@@ -171,6 +176,23 @@ function carregaListaDespesas( despesas = Array (), filtro = false) {
     	linha.insertCell(1).innerHTML = d.tipo
     	linha.insertCell(2).innerHTML = d.descricao
     	linha.insertCell(3).innerHTML = d.valor
+
+		// criar botão de exclusão
+		let btn = document.createElement("button")
+		btn.className = 'btn btn-danger'
+		btn.innerHTML = '<i class=" fas fa-times"></i>'
+		btn.id = `id_despesa_${d.id}`
+		btn.onclick = function() {			
+			let id = this.id.replace('id_despesa_','')
+
+			bd.remover(id)
+
+			window.location.reload()
+
+		}
+		linha.insertCell(4).append(btn)
+
+		console.log(d)
 
     	
     })	
